4dfb2c76d10816ed4a9d88eb6457ab99ca88d535
Marcus Battle <marcus@marcusbattle.com>
2021-10-03 00:56:12 -0400

    Added ability to forward email to sms

--- pages/api/receive-mail.js
+++ pages/api/receive-mail.js
@@ -3,6 +3,13 @@ import nextConnect from "next-connect";
 const multer = require('multer');
 const upload = multer()
 
+// Create Twilio object
+const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
+
 // Create the NextConnect Middleware
 const apiRoute = nextConnect({
   onError(error, req, res) {
@@ -16,11 +23,20 @@ const apiRoute = nextConnect({
 // Assign the Multer to the Middleware
 apiRoute.use(upload.any());
 
-// Do API logic
+// Forward the email as a text message
 apiRoute.post((req, res) => {
-  res.status(200).json({ data: 'success' });
-  console.log(req.body)
-  console.log('your email')
+
+  // Parse the email sent from SendGrid and convert to a text message
+  const textMessage = `Subject: ${req.body.subject}\nFrom: ${req.body.from}\n\n${req.body.text}`
+
+  // Send the text message
+  client.messages
+    .create({
+      body: textMessage,
+      to: process.env.FORWARDING_PHONE_NUMBER, // Text this number
+      from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
 });
 
 export default apiRoute;
